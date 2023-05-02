daf404241c37aee075c7418e858ecef72001bbb7
xchen <xchen601@gmail.com>
2023-05-01 20:13:04 -0400

    added form to contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,15 @@ import './css/Contact.css'
 function Contact(){
     return (
         <div className='contact-section' id='contact'>
-        <h2>Contact Me!</h2>
-        <p>Email: xchen110@gmail.com</p>
-    </div>
+            <h2>Contact Me!</h2>
+            <p>Email: xchen110@gmail.com</p>
+            <form>
+                <input type="text" id="name" name="name" placeholder="Name"></input>
+                <input type="text" id="email" name="email" placeholder="Email"></input>
+                <textarea id="subject" name="subject" placeholder="Write something... (Currently does not submit yet. Still a WIP. Will try to implement form functions soon)"></textarea>
+                <button type="submit" value="Submit">Submit</button>
+            </form>
+        </div>
     )
     
 }
